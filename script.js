553af7882d9bc5080c8ccad798a7af65dd3f69a2
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2023-10-22 20:04:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const timerEl = document.getElementById("timer");
+const startButtonEl = document.getElementById("start");
+const resetButtonEl = document.getElementById("reset");
+
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+
+//toggle time for check start and stop
+
+function toggleTimer() {
+    if (startButtonEl.textContent === "Start") {
+        startTime = Date.now() - elapsedTime;
+
+        timerInterval = setInterval(() => {
+            elapsedTime = Date.now() - startTime;
+            timerEl.textContent = formatTime(elapsedTime);
+        }, 10);
+
+        startButtonEl.textContent = "Pause";
+        stopButtonEl.disabled = false;
+    } else {
+        clearInterval(timerInterval);
+        startButtonEl.textContent = "Start";
+        stopButtonEl.disabled = true;
+    }
+}
+
+function formatTime(elapsedTime) {
+    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    return (
+        (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+        ":" +
+        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+        ":" +
+        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
+        "." +
+        (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+    );
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+
+    elapsedTime = 0;
+    timerEl.textContent = "00:00:00";
+
+    startButtonEl.disabled = false;
+}
+
+startButtonEl.addEventListener("click", toggleTimer);
+resetButtonEl.addEventListener("click", resetTimer);
