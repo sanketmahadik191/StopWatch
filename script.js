13a5e0b1c8934efcbc1bfd605e7feef7a9cf4ade
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-01-23 16:29:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,18 +27,15 @@ function toggleTimer() {
 }
 
 function formatTime(elapsedTime) {
-    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
     const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
     const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
     const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
     return (
         (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
-        ":" +
+        " : " +
         (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
-        ":" +
-        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
-        "." +
-        (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+        " : " +
+        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00")
     );
 }
 
@@ -46,7 +43,7 @@ function resetTimer() {
     clearInterval(timerInterval);
 
     elapsedTime = 0;
-    timerEl.textContent = "00:00:00";
+    timerEl.textContent = "00 : 00 : 00";
 
     startButtonEl.disabled = false;
 }
